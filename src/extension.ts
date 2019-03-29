f6a8b7860470ad1046a4d5dd8bf0f7995adc15cf
Alexey Duryagin <aiduryagih@gmail.com>
2019-03-29 16:40:33 +0300

    bugfixes

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import fetch from 'node-fetch';
+import debounce from 'lodash/debounce';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-
-let lastHeartbeat: number;
-let lastFile: string;
 
 const checkThatEnabled = (): { branch: string, project: string } => {
   const rootPath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.path : '';
@@ -25,9 +23,10 @@ export function activate(context: vscode.ExtensionContext) {
 
   if (projectData.branch && projectData.project) {
     const { branch, project } = projectData;
-    const enoughTimePassed = (time: number): boolean => {
-      return lastHeartbeat + 120000 < time;
-    };
+
+    const sendToAnton = debounce(() => {
+      fetch(encodeURI(`https://console.dialogflow.com/api-client/demo/embedded/52d9e889-9db3-477d-b0cd-e16039e07af6/demoQuery?q=пишу код для ${project} ${branch}&sessionId=1`));
+    }, 2000);
   
     const onEvent = () => {
       let editor = vscode.window.activeTextEditor;
@@ -39,22 +38,16 @@ export function activate(context: vscode.ExtensionContext) {
           let file: string = doc.fileName;
   
           if (file) {
-            let time: number = Date.now();
-  
-            if (enoughTimePassed(time)) {
-              fetch(`https://console.dialogflow.com/api-client/demo/embedded/52d9e889-9db3-477d-b0cd-e16039e07af6/demoQuery?q=пишу%20код%20для%${project}%20${branch}&sessionId=1`);
-              lastFile = file;
-              lastHeartbeat = time;
-            }
+            sendToAnton();
           }
         }
       }
     };
 
     const subscriptions: vscode.Disposable[] = [];
-    vscode.window.onDidChangeTextEditorSelection(onEvent, {}, subscriptions);
-    vscode.window.onDidChangeActiveTextEditor(onEvent, {}, subscriptions);
-    vscode.workspace.onDidSaveTextDocument(onEvent, {}, subscriptions);
+    context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(onEvent, {}, subscriptions));
+    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(onEvent, {}, subscriptions));
+    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(onEvent, {}, subscriptions));
   }
 }
 
